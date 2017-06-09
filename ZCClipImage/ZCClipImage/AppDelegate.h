bc2ff21a9be714f6631abcbb6479db5d6414f6c4
ZhangZhiChaoDavid <c.sponge@qq.com>
2017-06-09 14:26:41 +0800

    commit

---
+++ ZCClipImage/ZCClipImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCClipImage
+//
+//  Created by 张智超 on 2017/6/8.
+//  Copyright © 2017年 GeezerChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCClipImage/ZCClipImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZCClipImage
+//
+//  Created by 张智超 on 2017/6/8.
+//  Copyright © 2017年 GeezerChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
